1fa26b909403b833a69d30cf52b44fe718784ac6
GoareguerLucas <lucasp51@hotmail.fr>
2018-04-16 21:02:12 +0200

    Ajout de STLToPCD et de IncrementallyRegisterater

---
+++ STLToPCD/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+int main(int argc, char *argv[])
+{
+	
+	// pcl object
+	//pcl::PointCloud<pcl::PointXYZRGB> * mycloud = new pcl::PointCloud<pcl::PointXYZRGB>();
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr mycloud(new pcl::PointCloud<pcl::PointXYZRGB>);// = new pcl::PointCloud<pcl::PointXYZRGB>()
+	// vtk reader
+	vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
+	vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
+	
+	int i;
+	
+	for(i=1; i<argc; i++)
+	{
+		printf("%s\n",argv[i]);
+		reader->SetFileName(argv[i]);
+		reader->Update();
+
+		// convert vtk to pcl object
+		pcl::io::vtkPolyDataToPointCloud(polydata, *mycloud);
+		
+		// Sauvegarde du nuage au format PCD
+		std::stringstream ss;
+		ss << i-1 << ".pcd";
+		pcl::io::savePCDFile (ss.str (), *mycloud, true);
+	}
+	
+	
+	return 0;
+}
