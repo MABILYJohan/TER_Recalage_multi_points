5310b7404adde9ea0c977b984ee7b16982b9d129
Johan MABILY <Johan.MABILY@etu.univ-amu.fr>
2018-04-23 21:19:31 +0200

    jointICP +, mais problèmes avec le visualisateur

--- Tests/JointICP/main.cpp
+++ Tests/JointICP/main.cpp
@@ -1,6 +1,6 @@
-/******************
- * TEST JOINT ICP *
- ******************/
+/************
+ * TEST ICP *
+ ************/
 
 
 #include <iostream>
@@ -11,15 +11,19 @@
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/pcl_visualizer.h>
-//~ #include <pcl/console/time.h>   // TicToc
-
+//#include <pcl/console/time.h>   // TicToc
 #include <pcl/io/pcd_io.h>
-
 #include <pcl/filters/voxel_grid.h>
+
 
 #include <pcl/registration/joint_icp.h>
 #include <pcl/registration/correspondence_rejection_median_distance.h>
 #include <pcl/registration/correspondence_rejection_sample_consensus.h>
+
+
+using namespace pcl;
+using namespace pcl::io;
+using namespace std;
 
 
 /**
@@ -37,9 +41,9 @@ void print4x4Matrix (const Eigen::Matrix4f & matrix)
 }
 
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr load_cloud (char *fileName)
+PointCloud<PointXYZ>::Ptr loadCloud (char *fileName)
 {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr myCloud (new pcl::PointCloud<pcl::PointXYZ>);
+	PointCloud<PointXYZ>::Ptr myCloud (new PointCloud<PointXYZ>);
 	
 	std::string file = fileName;
 	std::size_t pos = file.find_last_of(".");
@@ -55,11 +59,11 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr load_cloud (char *fileName)
 		reader->Update();
 
 		// convert vtk to pcl object
-		pcl::io::vtkPolyDataToPointCloud(polydata, *myCloud);
+		io::vtkPolyDataToPointCloud(polydata, *myCloud);
 	}
 	else if(ext.compare(".pcd")==0) // Si c'est un fichier au format PCD
 	{
-		if (pcl::io::loadPCDFile<pcl::PointXYZ> (file, *myCloud) == -1) //* load the file
+		if (io::loadPCDFile<PointXYZ> (file, *myCloud) == -1) //* load the file
 		{
 			PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
 			exit (1);
@@ -74,14 +78,13 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr load_cloud (char *fileName)
 	return myCloud;
 }
 
-
-pcl::PointCloud<pcl::PointXYZ>::Ptr downSample_cloud (pcl::PointCloud<pcl::PointXYZ>::Ptr originalCloud)
+PointCloud<PointXYZ> downSample_cloud (PointCloud<PointXYZ>::Ptr originalCloud)
 {
-	pcl::PointCloud<pcl::PointXYZ>::Ptr reductCloud (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::VoxelGrid<pcl::PointXYZ> grid;
+	PointCloud<PointXYZ> reductCloud;
+	VoxelGrid<PointXYZ> grid;
 	grid.setLeafSize (0.5, 0.5, 0.5);
 	grid.setInputCloud (originalCloud);
-	grid.filter (*reductCloud);
+	grid.filter (reductCloud);
 	
 	return reductCloud;
 }
@@ -89,112 +92,59 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr downSample_cloud (pcl::PointCloud<pcl::Point
 
 void sampleRandomTransform (Eigen::Affine3f &trans, float max_angle, float max_trans)
 {
-    srand(0);
-    // Sample random transform
-    Eigen::Vector3f axis((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
-    axis.normalize();
-    float angle = (float)rand() / RAND_MAX * max_angle;
-    Eigen::Vector3f translation((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
-    translation *= max_trans;
-    Eigen::Affine3f rotation(Eigen::AngleAxis<float>(angle, axis));
-    trans = Eigen::Translation3f(translation) * rotation;
+	srand(0);
+	// Sample random transform
+	Eigen::Vector3f axis((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+	axis.normalize();
+	float angle = (float)rand() / RAND_MAX * max_angle;
+	Eigen::Vector3f translation((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+	translation *= max_trans;
+	Eigen::Affine3f rotation(Eigen::AngleAxis<float>(angle, axis));
+	trans = Eigen::Translation3f(translation) * rotation;
 }
+
 
 int main (int argc, char *argv[])
 {
 	if (argv[1] != NULL && strcmp(argv[1], "-h")==0) {
 		std::cout << " \nUsage  " << std::endl;
-		printf ("%s file1.stl file2.stl [maxCorDist] [iterations] [epsilon] [difDistEpsilon]\n\n", argv[0]);
+		printf ("%s file1.stl file2.stl\n\n", argv[0]);
 		exit (0);
 	}
 	
-	pcl::console::print_highlight ("Loading point clouds...\n");
+	console::print_highlight ("Loading point clouds...\n");
 	// Checking program arguments
 	if (argc-1 < 2)
 	{
 		printf ("Usage :\n");
-		printf ("\t\t%s file1.stl file2.stl [maxCorDist] [iterations] [epsilon] [difDistEpsilon]\n", argv[0]);
+		printf ("\t\t%s file1.stl file2.stl\n", argv[0]);
 		PCL_ERROR ("Provide two stl files.\n");
 		return (-1);
 	}
 	std::cout << " loading cloud src " << std::endl;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr BIGcloud_source = load_cloud(argv[1]);
+	PointCloud<PointXYZ>::Ptr BIGcloud_source = loadCloud(argv[1]);
 	std::cout << " loading cloud target " << std::endl;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr BIGcloud_target = load_cloud(argv[2]);
+	PointCloud<PointXYZ>::Ptr BIGcloud_target = loadCloud(argv[2]);
 	
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source_registered (new pcl::PointCloud<pcl::PointXYZ>); 
+	//
+	// Downsample for consistency and speed
+	// \note enable this for large datasets
+	console::print_highlight ("DownSampling...\n");
 	
-	//~ //
-	//~ // Downsample for consistency and speed
-	//~ // \note enable this for large datasets
-	//~ pcl::console::print_highlight ("DownSampling...\n");
-	//~ pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source = downSample_cloud (BIGcloud_source);
-	//~ pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_target = downSample_cloud (BIGcloud_target);
+	//~ PointCloud<PointXYZ>::Ptr cloud_source = downSample_cloud (BIGcloud_source);
+	//~ PointCloud<PointXYZ>::Ptr cloud_target = downSample_cloud (BIGcloud_target);
 	
-	
-	
-	pcl::PointCloud<pcl::PointXYZ> cloud_source, cloud_target, cloud_reg;
-	
-	// Set up
-	pcl::JointIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> reg;
-	reg.setMaximumIterations (50);
-	reg.setTransformationEpsilon (1e-8);
-	reg.setMaxCorrespondenceDistance (0.25); // Making sure the correspondence distance > the max translation
-	// Add a median distance rejector
-	pcl::registration::CorrespondenceRejectorMedianDistance::Ptr rej_med (new pcl::registration::CorrespondenceRejectorMedianDistance);
-	rej_med->setMedianFactor (4.0);
-	reg.addCorrespondenceRejector (rej_med);
-	// Also add a SaC rejector
-	pcl::registration::CorrespondenceRejectorSampleConsensus<pcl::PointXYZ>::Ptr rej_samp (new pcl::registration::CorrespondenceRejectorSampleConsensus<pcl::PointXYZ>);
-	reg.addCorrespondenceRejector (rej_samp);
-	
-	size_t ntransforms = 10;
-	for (size_t t = 0; t < ntransforms; t++)
-	{
-		
-		// Sample a fixed offset between cloud pairs
-		Eigen::Affine3f delta_transform;
-		// No rotation, since at a random offset this could make it converge to a wrong (but still reasonable) result
-		sampleRandomTransform (delta_transform, 0., 0.10);
-		// Make a few transformed versions of the data, plus noise
-		size_t nclouds = 5;
-		for (size_t i = 0; i < nclouds; i++)
-		{
-			pcl::PointCloud<pcl::PointXYZ>::ConstPtr source (cloud_source.makeShared ());
-			// Sample random global transform for each pair
-			Eigen::Affine3f net_transform;
-			sampleRandomTransform (net_transform, 2*M_PI, 10.);
-			// And apply it to the source and target
-			pcl::PointCloud<pcl::PointXYZ>::Ptr source_trans (new pcl::PointCloud<pcl::PointXYZ>);
-			pcl::PointCloud<pcl::PointXYZ>::Ptr target_trans (new pcl::PointCloud<pcl::PointXYZ>);
-			pcl::transformPointCloud (*source, *source_trans, delta_transform.inverse () * net_transform);
-			pcl::transformPointCloud (*source, *target_trans, net_transform);
-			// Add these to the joint solver
-			reg.addInputSource (source_trans);
-			reg.addInputTarget (target_trans);
-			
-		}
-		
-		// Register
-		reg.align (cloud_reg);
-		Eigen::Matrix4f trans_final = reg.getFinalTransformation ();
-		for (int y = 0; y < 4; y++) {
-			for (int x = 0; x < 4; x++)
-				EXPECT_NEAR (trans_final (y, x), delta_transform (y, x), 1E-2);
-		}
-		
-		EXPECT_TRUE (cloud_reg.empty ()); // By definition, returns an empty cloud
-		// Clear
-		reg.clearInputSources ();
-		reg.clearInputTargets ();
-	}
+	PointCloud<PointXYZ> cloud_source = downSample_cloud (BIGcloud_source);;
+	PointCloud<PointXYZ> cloud_target = downSample_cloud (BIGcloud_target);
+	PointCloud<PointXYZ> cloud_reg;
+	PointCloud<PointXYZRGBA> cloud_with_color;
 	
 	
+	////////////////////////////////////////////////////////////////////
+	// NORMAL //
 	
-	
-	
-	//~ pcl::console::print_highlight ("iterative closest point...\n");
-	//~ pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+	//~ console::print_highlight ("iterative closest point...\n");
+	//~ IterativeClosestPoint<PointXYZ, PointXYZ> icp;
 	//~ // Set the input source and target
 	//~ icp.setInputSource (cloud_source);
 	//~ icp.setInputTarget (cloud_target);
@@ -234,9 +184,82 @@ int main (int argc, char *argv[])
 	//~ print4x4Matrix (transformation);
 	
 	
+	////////////////////////////////////////////////////////////////////
+	// JOINT //
+	
+	console::print_highlight ("Jointiterative closest point...\n");
+	// Set up
+	JointIterativeClosestPoint<PointXYZ, PointXYZ> reg;
+	int maxIterations = 50;
+	reg.setMaximumIterations (maxIterations);
+	double epsilon = 1e-8;
+	reg.setTransformationEpsilon (epsilon);
+	double corDist = 0.25;
+	reg.setMaxCorrespondenceDistance (corDist); // Making sure the correspondence distance > the max translation
+	std::cout << " maxIterations(" << maxIterations <<")\n" 
+			<< " epsilon(" << epsilon <<")\n" 
+			<< " corDist(" << corDist <<")\n" 
+			<< std::endl;
+	// Add a median distance rejector
+	registration::CorrespondenceRejectorMedianDistance::Ptr rej_med (new registration::CorrespondenceRejectorMedianDistance);
+	rej_med->setMedianFactor (4.0);
+	reg.addCorrespondenceRejector (rej_med);
+	// Also add a SaC rejector
+	registration::CorrespondenceRejectorSampleConsensus<PointXYZ>::Ptr rej_samp (new registration::CorrespondenceRejectorSampleConsensus<PointXYZ>);
+	reg.addCorrespondenceRejector (rej_samp);
+	
+	size_t ntransforms = 10;
+	for (size_t t = 0; t < ntransforms; t++)
+	{
+		std::cout << " transform " << t << std::endl;
+		// Sample a fixed offset between cloud pairs
+		Eigen::Affine3f delta_transform;
+		// No rotation, since at a random offset this could make it converge to a wrong (but still reasonable) result
+		sampleRandomTransform (delta_transform, 0., 0.10);
+		// Make a few transformed versions of the data, plus noise
+		size_t nclouds = 5;
+		for (size_t i = 0; i < nclouds; i++)
+		{
+			PointCloud<PointXYZ>::ConstPtr source (cloud_source.makeShared ());
+			// Sample random global transform for each pair
+			Eigen::Affine3f net_transform;
+			sampleRandomTransform (net_transform, 2*M_PI, 10.);
+			// And apply it to the source and target
+			PointCloud<PointXYZ>::Ptr source_trans (new PointCloud<PointXYZ>);
+			PointCloud<PointXYZ>::Ptr target_trans (new PointCloud<PointXYZ>);
+			transformPointCloud (*source, *source_trans, delta_transform.inverse () * net_transform);
+			transformPointCloud (*source, *target_trans, net_transform);
+			// Add these to the joint solver
+			reg.addInputSource (source_trans);
+			reg.addInputTarget (target_trans);
+		}
+		
+		// Register
+		reg.align (cloud_reg);
+		Eigen::Matrix4f trans_final = reg.getFinalTransformation ();
+		//~ for (int y = 0; y < 4; y++)
+			//~ for (int x = 0; x < 4; x++)
+				//~ EXPECT_NEAR (trans_final (y, x), delta_transform (y, x), 1E-2);
+		
+		print4x4Matrix(trans_final);
+		
+		//~ EXPECT_TRUE (cloud_reg.empty ()); // By definition, returns an empty cloud
+		
+		// Clear
+		reg.clearInputSources ();
+		reg.clearInputTargets ();
+	}
+	
+	////////////////////////////////////////////////////////////////////
+	
+	
 	
 	//~ // Visualization
 	//~ pcl::visualization::PCLVisualizer viewer ("ICP");
+	
+	//~ PointCloud<PointXYZ>* cs = &cloud_source;
+	//~ PointCloud<PointXYZ>* ct = &cloud_target;
+	//~ PointCloud<PointXYZ>::Ptr cr = &cloud_reg;
 	
 	//~ // Create two vertically separated viewports
 	//~ int v1 (0);
@@ -246,28 +269,28 @@ int main (int argc, char *argv[])
 	
 	//~ float bckgr_gray_level = 0.0;  // Black
 	//~ float txt_gray_lvl = 1.0 - bckgr_gray_level;
-	//~ // Original point cloud (source) is white
+	// Original point cloud (source) is white
 	//~ pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_source_color_h 
-		//~ (cloud_source,
+		//~ (cs,
 		 //~ (int) 255 * txt_gray_lvl,
 		 //~ (int) 255 * txt_gray_lvl, 
 		 //~ (int) 255 * txt_gray_lvl);
-	//~ viewer.addPointCloud (cloud_source, cloud_source_color_h, "cloud_source v1", v1 );
-	//~ viewer.addPointCloud (cloud_source, cloud_source_color_h, "cloud_source v2", v2);
+	//~ viewer.addPointCloud (cs, cloud_source_color_h, "cloud_source v1", v1 );
+	//~ viewer.addPointCloud (cs, cloud_source_color_h, "cloud_source v2", v2);
 	
 	//~ // target cloud is green
-	//~ pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_target_color_h (cloud_target, 20, 180, 20);
-	//~ viewer.addPointCloud (cloud_target, cloud_target_color_h, "cloud_target_v1", v1);
+	//~ pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_target_color_h (ct, 20, 180, 20);
+	//~ viewer.addPointCloud (ct, cloud_target_color_h, "cloud_target_v1", v1);
 	
 	//~ // ICP aligned point cloud is red
-	//~ pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_icp_color_h (cloud_source_registered, 180, 20, 20);
-	//~ viewer.addPointCloud (cloud_source_registered, cloud_icp_color_h, "cloud_icp_v2", v2);
+	//~ pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> cloud_reg_color_h (cr, 180, 20, 20);
+	//~ viewer.addPointCloud (cr, cloud_reg_color_h, "cloud_icp_v2", v2);
 	
 	//~ // Adding text descriptions in each viewport
 	//~ viewer.addText ("White: cloud source\nGreen: cloud target", 10, 15, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "icp_info_1", v1);
 	//~ viewer.addText ("White: cloud source\nRed: ICP aligned point cloud", 10, 15, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "icp_info_2", v2);	
 	//~ std::stringstream ss;
-	//~ ss << iterations;
+	//~ ss << maxIterations;
 	//~ std::string iterations_cnt = "ICP iterations = " + ss.str ();
 	//~ viewer.addText (iterations_cnt, 10, 60, 16, txt_gray_lvl, txt_gray_lvl, txt_gray_lvl, "iterations_cnt", v2);
 	
