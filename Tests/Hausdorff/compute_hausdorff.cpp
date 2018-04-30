95d5e6499ffc238d5bcc8d6a7a464e5e4a8a001f
Johan MABILY <Johan.MABILY@etu.univ-amu.fr>
2018-04-30 13:56:37 +0200

    Hausdorff programme fonctionel

--- Tests/Hausdorff/compute_hausdorff.cpp
+++ Tests/Hausdorff/compute_hausdorff.cpp
@@ -45,6 +45,9 @@
 #include <pcl/console/time.h>
 #include <pcl/search/kdtree.h>
 
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/filters/voxel_grid.h>
+
 using namespace std;
 using namespace pcl;
 using namespace pcl::io;
@@ -56,22 +59,72 @@ typedef PointCloud<PointXYZ> Cloud;
 
 void printHelp (int, char **argv)
 {
-	print_error ("Syntax is: %s cloud_a.pcd cloud_b.pcd\n", argv[0]);
+	print_error ("Syntax is: %s cloud_a.pcd/stl cloud_b.pcd/stl\n", argv[0]);
 }
 
-bool loadCloud (const std::string &filename, Cloud &cloud)
+//~ bool loadCloud (const std::string &filename, Cloud &cloud)
+//~ {
+	//~ TicToc tt;
+	//~ print_highlight ("Loading "); print_value ("%s ", filename.c_str ());
+	
+	//~ tt.tic ();
+	//~ if (loadPCDFile (filename, cloud) < 0)
+		//~ return (false);
+	//~ print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
+	//~ print_info ("Available dimensions: "); print_value ("%s\n", pcl::getFieldsList (cloud).c_str ());
+	
+	//~ return (true);
+//~ }
+
+
+Cloud::Ptr loadCloud (char *fileName)
 {
-	TicToc tt;
-	print_highlight ("Loading "); print_value ("%s ", filename.c_str ());
+	Cloud::Ptr myCloud (new Cloud);
 	
-	tt.tic ();
-	if (loadPCDFile (filename, cloud) < 0)
-		return (false);
-	print_info ("[done, "); print_value ("%g", tt.toc ()); print_info (" ms : "); print_value ("%d", cloud.width * cloud.height); print_info (" points]\n");
-	print_info ("Available dimensions: "); print_value ("%s\n", pcl::getFieldsList (cloud).c_str ());
+	string file = fileName;
+	size_t pos = file.find_last_of(".");
+	string ext = file.substr(pos);
+	cout << ext << endl;
 	
-	return (true);
+	if(ext.compare(".stl") == 0) // Si c'est un fichier au format STL
+	{
+		// vtk reader
+		vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
+		vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
+		reader->SetFileName(fileName);
+		reader->Update();
+
+		// convert vtk to pcl object
+		pcl::io::vtkPolyDataToPointCloud(polydata, *myCloud);
+	}
+	else if(ext.compare(".pcd")==0) // Si c'est un fichier au format PCD
+	{
+		if (pcl::io::loadPCDFile<PointType> (file, *myCloud) == -1) //* load the file
+		{
+			PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+			exit (1);
+		}
+	}
+	else
+	{
+		std::cout << "Les format supportés sont STL et PCD" << std::endl;
+		exit(1);
+	}
+	
+	return myCloud;
 }
+
+Cloud::Ptr downSample_cloud (Cloud::Ptr originalCloud)
+{
+	Cloud::Ptr reductCloud (new Cloud);
+	pcl::VoxelGrid<PointType> grid;
+	grid.setLeafSize (0.5, 0.5, 0.5);
+	grid.setInputCloud (originalCloud);
+	grid.filter (*reductCloud);
+	
+	return reductCloud;
+}
+
 
 void compute (Cloud &cloud_a, Cloud &cloud_b)
 {
@@ -132,25 +185,40 @@ int main (int argc, char** argv)
 		return (-1);
 	}
 	
+	pcl::console::print_highlight ("Loading point clouds...\n");
+	
 	// Parse the command line arguments for .pcd files
 	std::vector<int> p_file_indices;
 	p_file_indices = parse_file_extension_argument (argc, argv, ".pcd");
+	p_file_indices = parse_file_extension_argument (argc, argv, ".stl");
 	if (p_file_indices.size () != 2)
 	{
-		print_error ("Need two PCD files to compute Hausdorff distance.\n");
+		print_error ("Need two PCD or STL files to compute Hausdorff distance.\n");
 		return (-1);
 	}
 	
 	// Load the first file
-	Cloud::Ptr cloud_a (new Cloud);
-	if (!loadCloud (argv[p_file_indices[0]], *cloud_a))
-		return (-1);
+	cout << " loading cloud a " << endl;
+	Cloud::Ptr Bigcloud_a = loadCloud(argv[1]);
+	//~ Cloud::Ptr cloud_a (new Cloud);
+	//~ if (!loadCloud (argv[p_file_indices[0]], *cloud_a))
+		//~ return (-1);
 	
 	// Load the second file
-	Cloud::Ptr cloud_b (new Cloud);
-	if (!loadCloud (argv[p_file_indices[1]], *cloud_b))
-		return (-1);
+	cout << " loading cloud target " << endl;
+	Cloud::Ptr Bigcloud_b = loadCloud(argv[2]);;
+	//~ if (!loadCloud (argv[p_file_indices[1]], *cloud_b))
+		//~ return (-1);
+	
+	//
+	// Downsample for consistency and speed
+	// \note enable this for large datasets
+	pcl::console::print_highlight ("DownSampling...\n");
+	Cloud::Ptr cloud_a = downSample_cloud (Bigcloud_a);
+	Cloud::Ptr cloud_b = downSample_cloud (Bigcloud_b);
+	
 	
 	// Compute the Hausdorff distance
+	pcl::console::print_highlight ("Hausdorff...\n");
 	compute (*cloud_a, *cloud_b);
 }
