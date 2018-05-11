e11f33dad328d39758b50194ab23e4c1ed481799
Johan MABILY <Johan.MABILY@etu.univ-amu.fr>
2018-05-11 18:44:28 +0200

    ICPWithNormals ready to test

--- Tests/ICPWithNormal/main.cpp
+++ Tests/ICPWithNormal/main.cpp
@@ -1,6 +1,6 @@
-/***********************
- * TEST ICPWithNormals *
- ***********************/
+/**********************
+ * TEST ICPWITHNORMAL *
+ **********************/
 
 
 #include <iostream>
@@ -8,16 +8,11 @@
 
 #include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
-//~ #include <pcl/io/vtk_lib_io.h>
 #include <pcl/registration/registration.h>
 #include <pcl/registration/icp.h>
-//~ #include <pcl/visualization/pcl_visualizer.h>
-//#include <pcl/console/time.h>   // TicToc
+#include <pcl/console/time.h>   // TicToc
+
 #include <pcl/io/pcd_io.h>
-//~ #include <pcl/filters/voxel_grid.h>
-
-
-//~ #include <gtest/gtest.h>
 
 #include <pcl/registration/transformation_estimation_point_to_plane.h>
 #include <pcl/registration/transformation_validation_euclidean.h>
@@ -28,26 +23,10 @@
 
 #include "utils_pcl.h"
 
+using namespace std;
 using namespace pcl;
 using namespace pcl::io;
-using namespace std;
 
-
-template <typename PointSource, typename PointTarget>
-class RegistrationWrapper : public Registration<PointSource, PointTarget>
-{
-	public:
-	void computeTransformation (pcl::PointCloud<PointSource> &, const Eigen::Matrix4f&) { }
-	
-	bool hasValidFeaturesTest ()
-	{
-		return (this->hasValidFeatures ());
-	}
-	void findFeatureCorrespondencesTest (int index, std::vector<int> &correspondence_indices)
-	{
-		this->findFeatureCorrespondences (index, correspondence_indices);
-	}
-};
 
 
 /**
@@ -65,151 +44,146 @@ void print4x4Matrix (const Eigen::Matrix4f & matrix)
 }
 
 
-void sampleRandomTransform (Eigen::Affine3f &trans, float max_angle, float max_trans)
-{
-	srand(0);
-	// Sample random transform
-	Eigen::Vector3f axis((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
-	axis.normalize();
-	float angle = (float)rand() / RAND_MAX * max_angle;
-	Eigen::Vector3f translation((float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
-	translation *= max_trans;
-	Eigen::Affine3f rotation(Eigen::AngleAxis<float>(angle, axis));
-	trans = Eigen::Translation3f(translation) * rotation;
-}
-
-
 int main (int argc, char *argv[])
 {
+	
 	if (argv[1] != NULL && strcmp(argv[1], "-h")==0) {
 		std::cout << " \nUsage  " << std::endl;
-		printf ("%s file1.stl/pcd file2.stl/pcd [ksearch]\n\n", argv[0]);
+		printf ("%s file1.stl/pcd file2.stl/pcd [deep of Ktree] [maxCorDist] [iterations] [epsilon] [difDistEpsilon]\n\n", argv[0]);
 		exit (0);
 	}
 	
-	console::print_highlight ("Loading point clouds...\n");
+	pcl::console::print_highlight ("Loading point clouds...\n");
 	// Checking program arguments
 	if (argc-1 < 2)
 	{
 		printf ("Usage :\n");
-		printf ("\t\t%s file1.stl/pcd file2.stl/pcd [ksearch]\n", argv[0]);
+		printf ("%s file1.stl/pcd file2.stl/pcd [deep of Ktree] [maxCorDist] [iterations] [epsilon] [difDistEpsilon]\n\n", argv[0]);
 		PCL_ERROR ("Provide two stl or pcd files.\n");
 		return (-1);
 	}
 	std::cout << " loading cloud src " << std::endl;
-	PointCloud<PointXYZ>::Ptr BIGcloud_source = loadCloud(argv[1]);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr BIGcloud_source = loadCloud(argv[1]);
 	std::cout << " loading cloud target " << std::endl;
-	PointCloud<PointXYZ>::Ptr BIGcloud_target = loadCloud(argv[2]);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr BIGcloud_target = loadCloud(argv[2]); 
 	
 	//
 	// Downsample for consistency and speed
 	// \note enable this for large datasets
-	console::print_highlight ("DownSampling...\n");
+	pcl::console::print_highlight ("DownSampling...\n");
+	pcl::PointCloud<pcl::PointXYZ> cloud_source = downSample_cloud (BIGcloud_source);
+	pcl::PointCloud<pcl::PointXYZ> cloud_target = downSample_cloud (BIGcloud_target);
 	
-	PointCloud<PointXYZ> cloud_source = downSample_cloud (BIGcloud_source);
-	PointCloud<PointXYZ> cloud_target = downSample_cloud (BIGcloud_target);
-	PointCloud<PointXYZ> cloud_reg;
-	PointCloud<PointXYZRGBA> cloud_with_color;
+	///////////////////////////////////////////////////////////////////
+	// PARAMS //
 	
+	PointCloud<PointNormal>::Ptr nsrc (new PointCloud<PointNormal>);
+	copyPointCloud (cloud_source, *nsrc);
+	PointCloud<PointNormal>::Ptr ntgt (new PointCloud<PointNormal>);
+	copyPointCloud (cloud_target, *ntgt);
 	
-	////////////////////////////////////////////////////////////////////
-	// ICP W NORMAL //
+	pcl::PointCloud<pcl::PointNormal>::Ptr cloud_nicp (new PointCloud<PointNormal>);
+	copyPointCloud (*nsrc, *cloud_nicp);
 	
-	console::print_highlight ("titerative closest point with normals...\n");
+	pcl::console::TicToc time;
+	time.tic ();
 	
-	typedef PointNormal PointT;
-	PointCloud<PointT>::Ptr src (new PointCloud<PointT>);
-	copyPointCloud (cloud_source, *src);
-	PointCloud<PointT>::Ptr tgt (new PointCloud<PointT>);
-	copyPointCloud (cloud_target, *tgt);
-	PointCloud<PointT> output;
-	
-	std::cout << " settings normalEstimationPoint" << std::endl;
+	pcl::console::print_highlight ("Normal Estimation...\n");
+	// Create the normal estimation class
 	NormalEstimation<PointNormal, PointNormal> norm_est;
+	// Create an empty kdtree representation, and pass it to the normal estimation object.
+	// Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
 	norm_est.setSearchMethod (search::KdTree<PointNormal>::Ptr (new search::KdTree<PointNormal>));
-	int kSearch = 10;
-	if (argv[3]!=NULL && atoi(argv[3])>0)	kSearch = atoi(argv[3]);
-	norm_est.setKSearch (kSearch);
-	std::cout << " kSearch (" << kSearch << ")\n" <<  std::endl;
-	norm_est.setInputCloud (tgt);
-	norm_est.compute (*tgt);
+	
+	int k = 10;
+	if (argv[3]!=NULL && atoi(argv[3])>0)	k = atoi(argv[3]);
+	norm_est.setKSearch (k);
+	// Pass the input dataset
+	norm_est.setInputCloud (ntgt);
+	// Compute the features
+	norm_est.compute (*ntgt);
 	
 	
-	std::cout << " settings IterativeClosestPoint" << std::endl;
-	IterativeClosestPoint<PointT, PointT> reg;
-	typedef registration::TransformationEstimationPointToPlane<PointT, PointT> PointToPlane;
+	typedef registration::TransformationEstimationPointToPlane<PointNormal, PointNormal> PointToPlane;
 	boost::shared_ptr<PointToPlane> point_to_plane (new PointToPlane);
-	reg.setTransformationEstimation (point_to_plane);
-	reg.setInputSource (src);
-	reg.setInputTarget (tgt);
-	int maxIterations = 50;
-	reg.setMaximumIterations (maxIterations);
+	
+	
+	
+	///////////////////////////////////////////////////////////////////
+	// NICP //
+	
+	pcl::console::print_highlight ("iterative closest point with normals...\n");
+	pcl::IterativeClosestPointWithNormals<pcl::PointNormal, pcl::PointNormal > nicp;
+	
+	// Set the max correspondence distance to 1m (e.g., correspondences with higher distances will be ignored)
+	double maxCorDist = 1;
+	if (argv[4]!=NULL && atof(argv[4])>0)	maxCorDist = atof(argv[4]);
+	nicp.setMaxCorrespondenceDistance (maxCorDist);
+	
+	// Set the maximum number of iterations (criterion 1)
+	int iterations = 5;
+	if (argv[5]!=NULL && atoi(argv[5])>0)	iterations = atoi(argv[5]);
+	nicp.setMaximumIterations (iterations);
+	
+	// Set the transformation epsilon (criterion 2)
 	double epsilon = 1e-8;
-	reg.setTransformationEpsilon (epsilon);
-	std::cout << " maxIterations(" << maxIterations <<")\n" 
-			<< " epsilon(" << epsilon <<")\n" 
-			<< std::endl;
+	if (argv[6]!=NULL && atof(argv[6])>0)	epsilon = atof(argv[6]);
+	nicp.setTransformationEpsilon (epsilon);
+	
+	// Set the euclidean distance difference epsilon (criterion 3)
+	double difDistEpsilon = 1;
+	if (argv[7]!=NULL && atoi(argv[7])>0)	difDistEpsilon = atoi(argv[7]);
+	nicp.setEuclideanFitnessEpsilon (difDistEpsilon);
+	
+	// Set the input source and target
+	nicp.setInputSource (cloud_nicp);
+	nicp.setInputTarget (ntgt);
+	
+	nicp.setTransformationEstimation (point_to_plane);
+	nicp.setInputSource (nsrc);
+	nicp.setInputTarget (ntgt);
 	// Use a correspondence estimator which needs normals
-	registration::CorrespondenceEstimationNormalShooting<PointT, PointT, PointT>::Ptr ce (new registration::CorrespondenceEstimationNormalShooting<PointT, PointT, PointT>);
-	reg.setCorrespondenceEstimation (ce);
-	// Add rejector
-	registration::CorrespondenceRejectorSurfaceNormal::Ptr rej (new registration::CorrespondenceRejectorSurfaceNormal);
-	rej->setThreshold (0); //Could be a lot of rotation -- just make sure they're at least within 0 degrees
-	reg.addCorrespondenceRejector (rej);
+	registration::CorrespondenceEstimationNormalShooting<PointNormal, PointNormal, PointNormal>::Ptr ce (new registration::CorrespondenceEstimationNormalShooting<PointNormal, PointNormal, PointNormal>);
+	nicp.setCorrespondenceEstimation (ce);
+	//~ // Add rejector
+	//~ registration::CorrespondenceRejectorSurfaceNormal::Ptr rej (new registration::CorrespondenceRejectorSurfaceNormal);
+	//~ rej->setThreshold (0); //Could be a lot of rotation -- just make sure they're at least within 0 degrees
+	//~ nicp.addCorrespondenceRejector (rej);
 	
 	
-	// Register
-	std::cout << " Register" << std::endl;
-	reg.align (output);
-	if (!reg.hasConverged ()) {
-		PCL_ERROR ("\nreg has not converged.\n");
+	// Perform the alignment
+	pcl::console::print_highlight ("Alignement...\n");
+	nicp.align (*cloud_nicp);
+	if (!nicp.hasConverged ()) {
+		PCL_ERROR ("\nICP has not converged.\n");
 		return (-1);
 	}
 	
-	//~ // Check again, for all possible caching schemes
-	//~ std::cout << " Check" << std::endl;
-	//~ for (int iter = 0; iter < 4; iter++)
-	//~ {
-		//~ bool force_cache = (bool) iter/2;
-		//~ bool force_cache_reciprocal = (bool) iter%2;
-		//~ pcl::search::KdTree<PointT>::Ptr tree(new pcl::search::KdTree<PointT>);
-		//~ // Ensure that, when force_cache is not set, we are robust to the wrong input
-		//~ if (force_cache)
-			//~ tree->setInputCloud (tgt);
-		//~ reg.setSearchMethodTarget (tree, force_cache);
-		
-		//~ pcl::search::KdTree<PointT>::Ptr tree_recip (new pcl::search::KdTree<PointT>);
-		//~ if (force_cache_reciprocal)
-			//~ tree_recip->setInputCloud (src);
-		//~ reg.setSearchMethodSource (tree_recip, force_cache_reciprocal);
-		
-		//~ // Register
-		//~ std::cout << " \tRegister" << std::endl;
-		//~ reg.align (output);
-		//~ if (!reg.hasConverged ()) {
-			//~ printf ("\treg has not converged.\n");
-			//~ continue;
-		//~ }
-	//~ }
+	//~ std::cout << " \nNICP has converged in " << time.toc () << "ms, score is  " << nicp.getFitnessScore () << std::endl;
+	print_info ("NICP has converged with score of "); print_value ("%f", nicp.getFitnessScore ()); print_info (" in "); print_value ("%g", time.toc ()); print_info (" ms : \n");
 	
-	printf ("reg has converged with score of %f\n", reg.getFitnessScore ());
+	Eigen::Matrix4f trans_final = nicp.getFinalTransformation();
+	std::cout << " FINAL " << std::endl;
+	print4x4Matrix(trans_final);
+	
 	
 	////////////////////////////////////////////////////////////////////
+	// OUTPUT //
 	
+	PointCloud<PointXYZ>::Ptr nicp_vizu (new PointCloud<PointXYZ>);
+	copyPointCloud (*cloud_nicp, *nicp_vizu);
+
+	//pcl::transformPointCloud(cloud_source, *cloud_nicp, trans_final);
 	
-	//~ // Visualization
+	// Compute the Hausdorff distance
+	pcl::console::print_highlight ("Hausdorff\n");
+	compute_Hausdorff (cloud_target, *nicp_vizu);
 	
-	PointCloud<PointXYZ> cloud_output;
-	copyPointCloud (output, cloud_output);
+	pcl::console::print_highlight ("Visualisation \n");
+	vizu (cloud_source, cloud_target, *nicp_vizu, iterations);
 	
-	vizu (cloud_source, cloud_target, cloud_output);
-	
-	return 0;
+	return (0);
 }
-
-
-
-
 
 
 
